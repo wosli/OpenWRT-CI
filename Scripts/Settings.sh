79ec7d44629f9bd7bad2a2b5d6718ecf5271b8c3
liq <64891712+wosli@users.noreply.github.com>
2024-02-28 00:10:16 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -44,3 +44,7 @@ fi
 #OpenWrt golang latest version
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
